406f2d80e7bb8b7140c04aa52b62a11671f77c3d
ArtBon <tyom.bond@gmail.com>
2018-08-21 12:48:35 +0300

    Added class methods

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class TStack{
-    static const int S_SIZE = 100;
+    static const int SIZ = 100;
     char *stack;
     unsigned len;
 public:
@@ -13,8 +13,30 @@ public:
     bool pop(char  &elem);
 };
 
+TStack::TStack(): stack(new char[SIZ]), len(0) {}
+
+bool TStack::push(char elem)
+{
+    if (len == SIZ) return 0;
+    stack[len++] = elem;
+    return 1;
+}
+
+bool TStack::pop(char &elem)
+{
+    if (len == 0) return 0;
+    elem = stack[--len];
+    return 1;
+}
+
+TStack::~TStack()
+{
+    delete []stack;
+    stack = nullptr;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
